c8c0c59911f046f807fb96bd6b56c904ae77d63c
Hewerton <hewerton.fl@gmail.com>
2023-10-15 14:28:17 -0300

    Ajustes de lógica

--- mestre.ino
+++ mestre.ino
@@ -1,20 +1,20 @@
 int data[] = { 1, 0, 0, 1, 0, 1, 1, 1, 0 };
-int arrayLenght = sizeof(data) / sizeof(data[0]);
+int dataLength = sizeof(data) / sizeof(data[0]);
 int outputPin = 23;
 
 void setup() {
   Serial.begin(9600);
   pinMode(outputPin, OUTPUT);
-  sender(data, arrayLenght);
+  sender(data, dataLength);
 }
 // Remetente
-void sender(int data[], int arrayLenght) {
-  for (int i = 0; i < arrayLenght; i++) {
+void sender(int data[], int dataLength) {
+  for (int i = 0; i < dataLength; i++) {
     digitalWrite(outputPin, data[i]);
     delay(1000);
   }
 }
 
 void loop() {
-  sender(data, arrayLenght);
+  sender(data, dataLength);
 }
