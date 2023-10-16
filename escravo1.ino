64aa149d6795abd0b18ce11b5cf55799fb59771c
Hewerton <hewerton.fl@gmail.com>
2023-10-16 20:44:40 -0300

    Organização de código do escravo

--- escravo1.ino
+++ escravo1.ino
@@ -1,22 +1,37 @@
-int inputPin = 2;
-int outputPin = 8;
+// Definição dos pinos
+const int inputPin = 2;
+const int outputPin = 8;
+
+// Definição do vetor de flag
 int flag[8] = { 0, 1, 1, 1, 1, 1, 1, 0 };
 int flagLength = sizeof(flag) / sizeof(flag[0]);
 bool openFlag = true;
 bool syncFlag = false;
+
+// Definição do vetor de dados auxiliar de comparação
 int auxRead[8];
+
+// Definição do vetor de dados recebidos
 int receivedData[8];
+
+// Contador auxiliar
 int auxCounter = 0;
-int timeClock = 100;
+
+// Tempo de transmissão de bit
+const int timeClock = 100;
+
+// Contagem de sincronização
 int syncCounter = 0;
 int counter = 0;
 
+// Configuração inicial do slave
 void setup() {
   Serial.begin(9600);
   pinMode(outputPin, OUTPUT);
   pinMode(inputPin, INPUT);
 }
 
+// Função de preenchimento do vetor auxiliar
 void fillArray() {
   if (auxCounter >= flagLength) {
     syncFlag = syncFlag ? true : sync(flag, auxRead);
@@ -31,6 +46,7 @@ void fillArray() {
   }
 }
 
+// Função de sincronização
 bool sync(int flag[], int auxRead[]) {
   for (int i = 0; i < flagLength; i++) {
     if (auxRead[i] != flag[i]) {
@@ -39,6 +55,8 @@ bool sync(int flag[], int auxRead[]) {
   }
   return true;
 }
+
+// Função de impressão dos dados recebidos
 void printReceivedData() {
   for (int i = 0; i < 8; i++) {
     Serial.print(receivedData[i]);
@@ -46,7 +64,7 @@ void printReceivedData() {
   Serial.println();
 }
 
-
+// Função de recebimento de dados
 void receiver() {
   if (!syncFlag){
     fillArray();
@@ -64,6 +82,7 @@ void receiver() {
   }
 }
 
+// Loop principal
 void loop() {
   int leitura = digitalRead(inputPin);
   digitalWrite(outputPin, leitura);
