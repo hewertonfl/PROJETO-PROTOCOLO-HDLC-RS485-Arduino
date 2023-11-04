aa1a55d9e3a714021f06387c26ce5088a493885c
Hewerton <hewerton.fl@gmail.com>
2023-11-04 16:33:43 -0300

    ajustes de sincronização

--- escravoteste.ino
+++ escravoteste.ino
@@ -36,10 +36,10 @@ int auxCounter = 0;
 bool stopFlag = false;
 // Definição da flag de sincronização
 bool syncFlag = false;
+bool sendingStatus = false;
 
 // Tempo de recepção de bit
 const int timeClock = 10;
-
 
 // Função de sincronização
 bool sync(int array[], int auxRead[], int length) {
@@ -49,12 +49,6 @@ bool sync(int array[], int auxRead[], int length) {
     }
   }
   return true;
-}
-
-void copyArray(int source[], int destination[], int length) {
-  for (int i = 0; i < length; i++) {
-    destination[i] = source[i];
-  }
 }
 
 // Função de preenchimento do vetor auxiliar
@@ -122,52 +116,56 @@ void checksumToBinaryArray(uint16_t checksum, int crc[], int crcLength) {
 
 // Função de recebimento de dados
 void receiver() {
-  if (!syncFlag) {
+  // Checagem de parada
+  if (stopFlag) { return; }
+  while (!syncFlag) {
     fillArray();
   }
-  if (!stopFlag) {
-    if (syncFlag) {
-      Serial.print("OpenFlag: ");
-      printReceivedData(receivedData,0, flagLength);
-      fillReceivedData(8);
-      // Checa se a mensagem é para mim
-      stopFlag = sync(adress, receivedData, adressLength);
-      if (stopFlag) {
-        Serial.print("Adress: ");
-        printReceivedData(receivedData, 0, adressLength);
-        Serial.print("Control: ");
-        fillReceivedData(8);
-        printReceivedData(receivedData, 0, controlLength);
-        // Checagem de erros
-        fillReceivedData(receivedDataLength);
-        int checksum = calculateChecksum(receivedData, crcLength, receivedDataLength-flagLength);
-        checksumToBinaryArray(checksum, crc, crcLength);
-        bool crcFlag = sync(crc, receivedData, crcLength);
-        bool sendingStatus = false;
-        if (crcFlag) {
-          Serial.println("CRC checked succeed!");
-          Serial.println("Data status: Good!");
-          // Print dos dados recebidos
-          Serial.print("Data received: ");
-          printReceivedData(receivedData, 0,receivedDataLength-flagLength);
-          // Print da closeFlag
-          Serial.print("CloseFlag: ");
-          printReceivedData(receivedData, receivedDataLength-flagLength,receivedDataLength);
-          int control[controlLength] = { 0, 0, 0, 0, 1, 1, 1, 1 };
-          if (!sendingStatus) {
-            sender(control, controlLength);
-          }
-        } else {
-          Serial.println("CRC error check!");
-          Serial.println("Data Status: Bad!");
-          digitalWrite(outputPin, 0);
-          int control[controlLength] = { 1, 1, 1, 1, 0, 0, 0, 0 };
-          sender(control, controlLength);
-        }
-      } else {
-        Serial.println("A msg não é para mim");
-      }
+  Serial.print("OpenFlag: ");
+  printReceivedData(receivedData,0, adressLength);
+  fillReceivedData(8);
+
+  // Checa se a mensagem é para mim
+  if (bool adressFlag = sync(adress, receivedData, adressLength)) {
+    Serial.print("Adress: ");
+    printReceivedData(receivedData, 0, adressLength);
+    Serial.print("Control: ");
+    fillReceivedData(8);
+    printReceivedData(receivedData, 0, flagLength);
+
+    // Checagem de erros
+    fillReceivedData(receivedDataLength);
+    int checksum = calculateChecksum(receivedData, crcLength, receivedDataLength - flagLength);
+    checksumToBinaryArray(checksum, crc, crcLength);
+    bool crcFlag = sync(crc, receivedData, crcLength);
+
+    if (!crcFlag) {
+      int control[controlLength] = { 1, 1, 1, 1, 0, 0, 0, 0 };
+      sender(control, controlLength);
+      //syncFlag = false;
+      return;
     }
+
+    Serial.println(crcFlag ? "CRC checked succeed!" : "CRC error check!");
+    Serial.println(crcFlag ? "Data status: Good!" : "Data Status: Bad!");
+
+    // Print dos dados recebidos
+    Serial.print("Data received: ");
+    printReceivedData(receivedData, 0, receivedDataLength - flagLength);
+
+    // Print da closeFlag
+    Serial.print("CloseFlag: ");
+    printReceivedData(receivedData, receivedDataLength - flagLength, receivedDataLength);
+    stopFlag = true;
+
+    sender(control, controlLength);
+    sendingStatus = false;
+    int control[controlLength] = { 0, 0, 0, 0, 1, 1, 1, 1 };
+    return;
+
+  } else {
+    syncFlag = false;
+    Serial.println("A msg não é para mim");
   }
 }
 // Configuração inicial do slave
@@ -179,7 +177,5 @@ void setup() {
 
 // Loop principal
 void loop() {
-  //int leitura = digitalRead(inputPin);
-  //digitalWrite(outputPin, leitura);
   receiver();
 }
