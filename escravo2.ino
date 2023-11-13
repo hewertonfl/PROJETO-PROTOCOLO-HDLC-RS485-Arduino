e732d805fd30260723db9ccc74f9c355c334220b
Hewerton <hewerton.fl@gmail.com>
2023-11-13 19:44:10 -0300

    Comunicação escravo - escravo

--- escravo2.ino
+++ escravo2.ino
@@ -7,15 +7,15 @@ const int outputPin = 23;
 // Definição do vetor de flag
 const int flagLength = 8;
 int flag[flagLength] = { 0, 1, 1, 1, 1, 1, 1, 0 };
-int counterSender = 0;
+
+// Definição do vetor de dados
+const int dataLength = 8;
+int data[dataLength] = { 1, 0, 0, 1, 0, 1, 1, 1 };
+
 
 // Definição do adress
 const int adressLength = 8;
-int myAdress[adressLength] = { 1, 1, 0, 1, 1, 0, 0, 1 };
-
-// Definição do vetor de dados recebidos
-const int receivedDataLength = 24;
-int receivedData[receivedDataLength];
+int myAdress[adressLength] = { 1, 1, 0, 0, 1, 1, 0, 1 };
 
 // Definição do vetor de controle de dados
 const int controlLength = 8;
@@ -24,6 +24,14 @@ int control[controlLength] = { 0, 0, 0, 0, 1, 1, 1, 1 };
 // Definição do vetor de CRC
 const int crcLength = 8;
 int crc[crcLength];
+
+// Definição do vetor de dados final
+const int finalDataLength = crcLength + dataLength;
+int finalData[finalDataLength];
+
+// Definição do vetor de dados recebidos
+const int receivedDataLength = crcLength + dataLength + flagLength;
+int receivedData[receivedDataLength];
 
 // ************************** Contadores ***************************** //
 // Contagem de sincronização
@@ -37,10 +45,14 @@ int auxCounter = 0;
 bool stopFlag = false;
 // Definição da flag de sincronização
 bool syncFlag = false;
-bool executou = false;
 bool sendingStatus = false;
+bool receivingStatus = false;
 bool crcFlag = false;
 bool resetFlag = false;
+bool questionFlag = true;
+bool masterMode = false;
+bool idleStatus = false;
+bool readStatus = false;
 
 // Tempo de recepção de bit
 const int timeClock = 10;
@@ -57,9 +69,32 @@ bool sync(int array[], int auxRead[], int length) {
 }
 
 // Remetente
-void sender(int data[], int dataLength) {
+void sender(int data[], int dataLength, int time = 0) {
   for (int i = 0; i < dataLength; i++) {
     digitalWrite(outputPin, data[i]);
+    //Serial.print(data[i]);
+    delay(timeClock);
+  }
+  delay(timeClock * time);
+  //Serial.println();
+}
+// Envia o frame
+void sendFrame(int adress[]) {
+  sender(flag, flagLength);
+  sender(adress, adressLength);
+  sender(control, controlLength);
+  sender(finalData, finalDataLength);
+  sender(flag, flagLength);
+}
+// Detecta se o dispositivo está lendo ou não
+void amIReading() {
+  for (int i = 0; i < 10; i++) {
+    int aux[8];
+    if (digitalRead(inputPin)) {
+      readStatus = true;
+      return;
+    }
+    readStatus = false;
     delay(timeClock);
   }
 }
@@ -107,6 +142,16 @@ void checksumToBinaryArray(uint16_t checksum, int crc[], int crcLength) {
   Serial.println();
 }
 
+// Função de conversão do checksum para binário
+void checksumToBinaryArrayMM(uint16_t checksum, int crc[], int crcLength) {
+  //Serial.print("CRC: ");
+  for (int i = crcLength - 1; i >= 0; i--) {
+    crc[i] = (checksum >> i) & 0x01;
+    //Serial.print(crc[i]);
+  }
+  //Serial.println();
+}
+
 // Função de recebimento de dados
 void receiver() {
   if (!stopFlag) {
@@ -118,7 +163,7 @@ void receiver() {
     }
     Serial.print("OpenFlag: ");
     printReceivedData(receivedData, 0, flagLength);
-    delay(8 * timeClock);
+    //delay(8 * timeClock);
     fillReceivedData(8);
     checksum += calculateChecksum(receivedData, 0, 8);
 
@@ -135,7 +180,7 @@ void receiver() {
       fillReceivedData(receivedDataLength);
       checksum += calculateChecksum(receivedData, crcLength, receivedDataLength - flagLength);
       checksumToBinaryArray(checksum, crc, crcLength);
-      crc[0] = 1;
+      //crc[0] = 1;
       crcFlag = sync(crc, receivedData, crcLength);
 
       if (crcFlag) {
@@ -159,20 +204,60 @@ void receiver() {
         Serial.println("Data Status: Bad!");
         Serial.print("CRC Received: ");
         printReceivedData(receivedData, 0, flagLength);
-        digitalWrite(outputPin, 1);
-        stopFlag = true;
+        //digitalWrite(outputPin, 1);
       }
       sendingStatus = true;
       Serial.println("Status: sending response...");
     } else {
       Serial.println("A msg não é para mim");
+      delay(8 * timeClock);
       syncFlag = false;
       checksum = 0;
     }
   }
 }
+void reset() {
+  stopFlag = false;
+  syncFlag = false;
+  sendingStatus = false;
+  crcFlag = false;
+  resetFlag = false;
+  readStatus = false;
+  digitalWrite(outputPin, 0);
+}
 
+void slave() {
+  if (!sendingStatus) {
+    receiver();
+  } else {
+    //Checa se o status da mensagem é good
+    int counter1 = 0;
+    while (crcFlag) {
+      pinMode(inputPin, INPUT_PULLDOWN);
+      pinMode(outputPin, OUTPUT);
+      int control[controlLength] = { 1, 0, 0, 0, 1, 1, 1, 0 };
+      sender(control, controlLength);
 
+      counter1++;
+      delay(timeClock);
+    }
+    //Checa se o status da mensagem é bad
+    int counter2 = 0;
+    while (!crcFlag && counter2<5) {
+      pinMode(inputPin, INPUT_PULLDOWN);
+       pinMode(outputPin, OUTPUT);
+      int control[controlLength] = { 0, 1, 1, 0, 1, 1, 0, 0 };
+      sender(control, controlLength);
+      resetFlag = true;
+
+      counter2++;
+      delay(timeClock);
+    }
+    if (resetFlag) {
+      reset();
+    }
+  }
+}
 // Função de impressão de vetor
 void printArray(int array[], int arrayLength) {
   for (int i = 0; i < arrayLength; i++) {
@@ -181,42 +266,125 @@ void printArray(int array[], int arrayLength) {
   Serial.println();
 }
 
-void reset() {
-  stopFlag = false;
-  syncFlag = false;
-  sendingStatus = false;
-  crcFlag = false;
-  resetFlag = false;
-  digitalWrite(outputPin, 0);
+// ****************** Master Mode **************** //
+int adress[adressLength] = { 1, 1, 0, 0, 0, 0, 0, 1 };
+// Função de cálculo do checksum
+uint16_t calculateChecksumMM(int data[], int length) {
+  uint16_t checksum = 0;
+  //Serial.print("Checksum: ");
+  for (int i = 0; i < length; i++) {
+    checksum += data[i];
+  }
+  //Serial.print(checksum);
+  return checksum;
 }
 
-// Configuração inicial do slave
+// Função de concatenação de vetores
+void concatenateArrays(int array1[], int array2[], int size1, int size2) {
+  // Copie os elementos do primeiro array para o vetor final
+  int counter = 0;
+  for (int i = size1 - 1; i >= 0; i--) {
+    finalData[counter] = array1[i];
+    counter++;
+  }
+
+  // Copie os elementos do segundo array para o vetor final
+  for (int i = 0; i < size2; i++) {
+    finalData[size1 + i] = array2[i];
+  }
+  //Serial.print("Final data: ");
+  //printArray(finalData, finalDataLength);
+}
+
+void master() {
+  if (!digitalRead(inputPin) && masterMode) {
+    Serial.println("Master Mode");
+    int checksum = calculateChecksumMM(data, 8) + calculateChecksumMM(adress, 8) + calculateChecksumMM(control, 8);
+    checksumToBinaryArrayMM(checksum, crc, crcLength);
+    concatenateArrays(crc, data, crcLength, dataLength);
+    sendFrame(adress);
+  } else {
+    pinMode(inputPin, INPUT_PULLUP);
+    Serial.println("Status: receiving response...");
+    fillReceivedData(8);
+    if (receivedData[0] && !receivedData[1] && !receivedData[2] && !receivedData[3] && receivedData[4] && receivedData[5] && receivedData[6] && !receivedData[7]) {
+      Serial.println("Frame sended!");
+      masterMode = false;
+      questionFlag = false;
+    }
+  }
+}
+
+void sendAck() {
+  int control[controlLength] = { 0, 0, 0, 0, 1, 0, 1, 0 };
+  Serial.println("sending ACK...");
+  for (int i = 0; i < 20; i++) { sender(control, controlLength, i); }
+}
+
+// ****************** Configuração inicial do slave **************** //
 void setup() {
   Serial.begin(115200);
   pinMode(inputPin, INPUT);
   pinMode(outputPin, OUTPUT);
-  //digitalWrite(outputPin, 0);
+  digitalWrite(outputPin, 0);
 }
 
-// Loop principal
+// ************************* Loop principal *************************** //
 void loop() {
-  if (!sendingStatus) {
-    receiver();
-    //Checa se o status da mensagem é good
-  } else if (crcFlag && !digitalRead(inputPin)) {
-    pinMode(inputPin, INPUT_PULLDOWN);
-    int control[controlLength] = { 1, 0, 0, 0, 1, 1, 1, 0 };
-    sender(control, controlLength);
-    //Checa se o status da mensagem é bad
-  } else if (!digitalRead(inputPin)) {
-    pinMode(inputPin, INPUT_PULLDOWN);
-    int control[controlLength] = { 1, 0, 0, 0, 1, 1, 1, 1 };
-    for(int i = 0; i<20;i++){sender(control, controlLength);}
-    pinMode(inputPin, INPUT);
-    resetFlag = true;
-  }
+  if (!idleStatus && questionFlag) {
+    readStatus = true;
+    while (!masterMode && !receivingStatus && readStatus) {
+      Serial.println("Sending request...");
+      pinMode(inputPin, INPUT_PULLDOWN);
+      int control[controlLength] = { 0, 0, 1, 1, 1, 0, 1, 0 };
+      sender(control, controlLength);
+      amIReading();
+    }
 
-  else if (digitalRead(inputPin) && resetFlag) {
-   reset();
- }
+    amIReading();
+    while (readStatus && !receivingStatus) {
+      fillReceivedData(8);
+      printArray(receivedData, 8);
+      if (receivedData[0] && receivedData[1] && receivedData[2] && !receivedData[3] && receivedData[4] && !receivedData[5] && receivedData[6] && !receivedData[7]) {
+        Serial.println("Request sended!");
+        printArray(receivedData, 8);
+        masterMode = true;
+        receivingStatus = true;
+        readStatus = false;
+      }
+    }
+
+    if (masterMode) {
+      Serial.println("Master Mode");
+      int checksum = calculateChecksumMM(data, 8) + calculateChecksumMM(adress, 8) + calculateChecksumMM(control, 8);
+      checksumToBinaryArrayMM(checksum, crc, crcLength);
+      concatenateArrays(crc, data, crcLength, dataLength);
+      sendFrame(adress);
+
+      amIReading();
+      while (readStatus) {
+        fillReceivedData(8);
+        printArray(receivedData, 8);
+        if (!receivedData[0] && !receivedData[1] && !receivedData[2] && !receivedData[3] && receivedData[4] && !receivedData[5] && receivedData[6] && !receivedData[7]) {
+          Serial.println("sended success!");
+          printArray(receivedData, 8);
+          idleStatus = true;
+          readStatus = false;
+        }
+        //delay(timeClock);
+      }
+    }
+  } else if (questionFlag) {
+    readStatus = false;
+    while (!readStatus) {
+      digitalWrite(outputPin, 1);
+      amIReading();
+    }
+    Serial.println("Acabou!");
+    digitalWrite(outputPin, 0);
+    questionFlag = false;
+  } else if (!questionFlag) {
+    pinMode(outputPin, INPUT_PULLDOWN);
+    slave();
+  }
 }
--- mestre.ino
+++ mestre.ino
@@ -1,11 +1,12 @@
 // ********************* Definição dos pinos ********************* //
 const int outputPin = 2;
 const int inputPin = 8;
+int option = 2;
 
 // ****************** Definição de vetores de bits **************** //
 // Definição do vetor de dados
 const int dataLength = 8;
-int data[dataLength] = { 1, 0, 0, 1, 0, 1, 1, 1 };
+int data[dataLength] = { 0, 0, 0, 0, 0, 1, 0, 1 };
 
 // Definição do vetor de flag
 const int flagLength = 8;
@@ -13,11 +14,12 @@ int flag[flagLength] = { 0, 1, 1, 1, 1, 1, 1, 0 };
 
 // Definição do vetor adress de destino
 const int adressLength = 8;
-int adress[adressLength] = { 1, 1, 0, 0, 0, 0, 0, 1 };
+int myAdress[adressLength] = { 1, 1, 0, 0, 0, 0, 0, 1 };
 
 // Definição do vetor de controle de dados
 const int controlLength = 8;
-int control[controlLength] = { 0, 0, 0, 0, 0, 0, 0, 1 };
+// Vetor de controle que pergunta para o escravo se ele quer se comunicar com o segundo escravo
+int control[controlLength] = { 0, 0, 0, 0, 0, 1, 0, 1 };
 
 // Definição do vetor de CRC
 const int crcLength = 8;
@@ -27,12 +29,37 @@ int crc[crcLength];
 const int finalDataLength = crcLength + dataLength;
 int finalData[finalDataLength];
 // Definição do vetor de dados recebidos
-const int receivedDataLength = 8;
+const int receivedDataLength = 24;
 int receivedData[receivedDataLength];
 
 // Tempo de recepção de bit
 const int timeClock = 10;
-int status = 0;
+bool sendingStatus = true;
+
+// ***************************** Flags **************************** //
+// Definição de status de print
+bool printStatus = false;
+bool dataStatus = false;
+bool stopFlag = false;
+bool syncFlag = false;
+bool crcFlag = false;
+bool resetFlag = false;
+bool questionFlag = true;
+bool slaveMode = false;
+bool requestFlag = false;
+bool frameReceived = false;
+bool readStatus = false;
+bool badStatus = false;
+// Remetente
+void sender(int data[], int dataLength, int time = 0) {
+  for (int i = 0; i < dataLength; i++) {
+    digitalWrite(outputPin, data[i]);
+    //Serial.print(data[i]);
+    delay(timeClock);
+  }
+  delay(timeClock * time);
+  //Serial.println();
+}
 
 // Função de impressão de vetor
 void printArray(int array[], int arrayLength) {
@@ -40,20 +67,6 @@ void printArray(int array[], int arrayLength) {
     Serial.print(array[i]);
   }
   Serial.println();
-}
-// ***************************** Flags **************************** //
-// Definição de status de print
-bool printStatus = false;
-bool response = false;
-bool dataStatus = false;
-// Remetente
-void sender(int data[], int dataLength) {
-  for (int i = 0; i < dataLength; i++) {
-    digitalWrite(outputPin, data[i]);
-    //Serial.print(data[i]);
-    delay(timeClock);
-  }
-  //Serial.println();
 }
 
 
@@ -101,15 +114,16 @@ void concatenateArrays(int array1[], int array2[], int size1, int size2) {
   //Serial.print("Final data: ");
   //printArray(finalData, finalDataLength);
 }
-
 // Função de preenchimento do vetor de dados
-void fillReceivedData(int receivedDataLength) {
+void fillReceivedData(int receivedDataLength, bool print = false) {
   int counter = 0;
   while (counter < receivedDataLength) {
+    delay(timeClock);
     receivedData[counter] = digitalRead(inputPin);
+    if (print) { Serial.print(receivedData[counter]); }
     counter++;
-    delay(10);
   }
+  if (print) { Serial.println(); }
 }
 
 // Configuração inicial do Arduino
@@ -120,44 +134,225 @@ void setup() {
 }
 
 
-void selectAdress(int option) {
+void selectAdress() {
   if (option == 1) {
+    // Escravo 1
     int adress[adressLength] = { 1, 1, 0, 0, 0, 0, 0, 1 };
     int checksum = calculateChecksum(data, 8) + calculateChecksum(adress, 8) + calculateChecksum(control, 8);
     checksumToBinaryArray(checksum, crc, crcLength);
     concatenateArrays(crc, data, crcLength, dataLength);
     sendFrame(adress);
   } else if (option == 2) {
-    int adress[adressLength] = { 1, 1, 0, 1, 1, 0, 0, 1 };
-    int checksum = calculateChecksum(data, 8) + calculateChecksum(adress, 8) + calculateChecksum(control, 8);
-    checksumToBinaryArray(checksum, crc, crcLength);
+    //Escravo 2
+    int adress[adressLength] = { 1, 1, 0, 0, 1, 1, 0, 1 };
+    int checksum  = calculateChecksum(data, 8) + calculateChecksum(adress, 8) + calculateChecksum(control, 8);
+    checksumToBinaryArray(checksum , crc, crcLength);
     concatenateArrays(crc, data, crcLength, dataLength);
     sendFrame(adress);
+    // Comunicação escravo-escravo
   } else if (option == 3) {
-    Serial.println("Em construção");
+    Serial.println("Status: sending received frame by master to slave...");
+    sendFrameSM();
   }
 }
-// Loop principal
-void loop() {
+void amIReading() {
+  for (int i = 0; i < 10; i++) {
+    int aux[8];
+    if (digitalRead(inputPin)) {
+      readStatus = true;
+      return;
+    }
+    readStatus = false;
+    delay(timeClock);
+  }
+}
+
+void master(int mode = 0) {
   if (!dataStatus) {
-    if (!digitalRead(inputPin)) {
-      Serial.println("Status: sending frame...");
-      selectAdress(1);
-    } else {
-      //if (!response) {
-      pinMode(inputPin, INPUT_PULLUP);
-      Serial.println("Status: receiving response...");
-      fillReceivedData(8);
-      if (receivedData[0] && !receivedData[1] && !receivedData[2] && !receivedData[3] && receivedData[4] && receivedData[5] && receivedData[6] && !receivedData[7]) {
-        Serial.println("Data status: Good!");
-        printArray(receivedData, 8);
-        dataStatus = true;
-      } else if (receivedData[0] && !receivedData[1] && !receivedData[2] && !receivedData[3] && receivedData[4] && receivedData[5] && receivedData[6] && receivedData[7]) {
-        Serial.println("Data status: Bad!");
-        printArray(receivedData, 8);
-        dataStatus = false;
-        pinMode(inputPin, INPUT);
+    if (mode == 0) {
+      amIReading();
+      while (readStatus) {
+        if(!badStatus){Serial.println("Status: receiving response...");}
+        //printArray(receivedData, 8);
+        fillReceivedData(8);
+        if (receivedData[0] && !receivedData[1] && !receivedData[2] && !receivedData[3] && receivedData[4] && receivedData[5] && receivedData[6] && !receivedData[7]) {
+          Serial.println("Data status: Good!");
+          printArray(receivedData, 8);
+          dataStatus = true;
+          readStatus = false;
+          return;
+        } else if (!receivedData[0] && receivedData[1] && receivedData[2] && !receivedData[3] && receivedData[4] && receivedData[5] && !receivedData[6] && !receivedData[7]) {
+          Serial.println("Data status: Bad!");
+          printArray(receivedData, 8);
+          pinMode(inputPin, INPUT);
+          badStatus = true;
+          readStatus = false;
+        } else if (!receivedData[0] && !receivedData[1] && receivedData[2] && receivedData[3] && receivedData[4] && !receivedData[5] && receivedData[6] && !receivedData[7]) {
+          Serial.println("Master change to slave mode!");
+          slaveMode = true;
+          readStatus = false;
+          return;
+        }
       }
+      Serial.println("Status: sending frame...");
+      selectAdress();
+    } else if (mode == 1) {
+      Serial.println("Status: sending frame...");
+      selectAdress();
     }
   }
 }
+
+// ************************** Slave MODE ***************************** //
+// Definição dos vetores recebidos
+int RopenFlag[flagLength];
+
+// Definição do vetor de dados
+int Rdata[dataLength];
+
+// Definição do adress
+int Radress[adressLength];
+
+// Definição do vetor de controle de dados
+int Rcontrol[controlLength];
+
+// Definição do vetor de CRC
+int Rcrc[crcLength];
+
+// Definição do vetor de dados final
+int RfinalData[finalDataLength];
+
+// Definição de vetor close flag
+int RcloseFlag[flagLength];
+
+// Função de sincronização
+bool sync(int array[], int auxRead[], int length) {
+  for (int i = 0; i < length; i++) {
+    if (array[i] != auxRead[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void fillVector(int vector[], int receivedData[], int length) {
+  for (int i = 0; i < length; i++) {
+    vector[i] = receivedData[i];
+  }
+}
+
+// Função de impressão dos dados recebidos
+void printReceivedData(int array[], int start, int arrayLength) {
+  for (int i = start; i < arrayLength; i++) {
+    Serial.print(array[i]);
+  }
+  Serial.println();
+}
+
+// Função de cálculo do checksum
+uint16_t calculateChecksum(int data[], int crcLength, int receivedDataLength) {
+  uint16_t checksum = 0;
+  //Serial.print("Checksum: ");
+  for (int i = crcLength; i < receivedDataLength; i++) {
+    checksum += data[i];
+  }
+  //Serial.println(checksum);
+  return checksum;
+}
+
+void sendFrameSM() {
+  sender(RopenFlag, flagLength);
+  sender(Radress, adressLength);
+  sender(Rcontrol, controlLength);
+  sender(RfinalData, finalDataLength);
+  sender(RcloseFlag, flagLength);
+}
+
+void printFrame() {
+  // Print da openFlag
+  Serial.print("OpenFlag: ");
+  printReceivedData(RopenFlag, 0, flagLength);
+  // Print do Adress
+  Serial.print("Adress: ");
+  printReceivedData(Radress, 0, adressLength);
+  // Print do controle
+  Serial.print("Control: ");
+  printReceivedData(Rcontrol, 0, controlLength);
+  // Print do CRC
+  Serial.print("CRC: ");
+  printReceivedData(RfinalData, 0, 8);
+  // Print dos dados recebidos
+  Serial.print("Data received: ");
+  printReceivedData(RfinalData, 8, 16);
+  // Print da closeFlag
+  Serial.print("CloseFlag: ");
+  printReceivedData(RcloseFlag, 0, flagLength);
+}
+
+// Função de recebimento de dados
+void receiver() {
+  if (!stopFlag) {
+    while (!syncFlag) {
+      fillReceivedData(8, true);
+      syncFlag = syncFlag ? true : sync(flag, receivedData, flagLength);
+    }
+    // Saving openFlag
+    fillVector(RopenFlag, receivedData, flagLength);
+    // Saving Adress
+    //delay(8 * timeClock);
+    fillReceivedData(8);
+    fillVector(Radress, receivedData, adressLength);
+    // Saving controle
+    fillReceivedData(8);
+    fillVector(Rcontrol, receivedData, adressLength);
+    // Saving CRC + Data
+    fillReceivedData(16);
+    fillVector(RfinalData, receivedData, finalDataLength);
+    // Saving closeFlag
+    fillReceivedData(8);
+    fillVector(RcloseFlag, receivedData, flagLength);
+    printFrame();
+    stopFlag = true;
+    frameReceived = true;
+    readStatus = false;
+  }
+}
+
+void sendAck(int control[], int index) {
+  Serial.print("sending ACK ");
+  Serial.print(index);
+  Serial.println("...");
+  sender(control, controlLength);
+}
+// Loop principal
+void loop() {
+  if (!frameReceived) {
+    while (slaveMode && !requestFlag && !frameReceived && !readStatus) {
+      int control[controlLength] = { 1, 1, 1, 0, 1, 0, 1, 0 };
+      sendAck(control, 1);
+      amIReading();
+    }
+    amIReading();
+    while (readStatus && slaveMode && !requestFlag) {
+      if (!stopFlag) { Serial.println("Slave Mode"); }
+      requestFlag = true;
+      receiver();
+    }
+  } else if (slaveMode) {
+    while (frameReceived && !readStatus) {
+      int control[controlLength] = { 0, 0, 0, 0, 1, 0, 1, 0 };
+      sendAck(control, 2);
+      amIReading();
+    }
+    readStatus = false;
+    slaveMode = false;
+  }
+  if (!slaveMode && frameReceived) {
+    option = 3;
+    master(1);
+  } else {
+    master();
+  }
+}
+
+//}
